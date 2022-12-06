2b9d0a37ccfd4e927b8a2e77fc21b0a66b765307
John Davies <jd.other@hotmail.com>
2022-12-06 18:31:51 +0000

    set hash dynamically from user input (search parameters)

--- src/Pages/Results/Block/Block.js
+++ src/Pages/Results/Block/Block.js
@@ -1,19 +1,18 @@
 import React, { useEffect } from 'react'
-// import { useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import { getBlock } from '../../../Utils/Api/http'
 
 const Block = () => {
-  // const [userInput] = useSearchParams({})
-  const hash = "00000000000000000003b5a5d4ca8b03fbae35d81df062b818b8116e4525cb23"
+  const [userInput] = useSearchParams({})
   
   useEffect(() => {
-    // console.log('userInput: ', userInput.get("userInput"))
+    console.log('userInput: ', userInput.get("userInput"))
+    const hash = userInput.get("userInput")
 
     getBlock(hash).then(
       result => console.log('result', result)
     )
-  }, [])
-  // }, [userInput]);
+  }, [userInput]);
   
   return (
     <>
