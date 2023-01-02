8abd1a400249735f80da1562ba6e49a67ededd99
John Davies <jd.other@hotmail.com>
2023-01-02 19:41:30 +0000

    added back link and basic styling to error page

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import back from '../../Assets/Images/Icons/back.svg'
 
 const Error = () => {
   return (
     <>
-        <h1>:-(</h1>
-        <h2>Oops! Something went wrong.</h2>
-        <p>Try refreshing the page later.</p>
+        <Link to="/">
+            <img className="h-[50px] w-[50px] mt-[20px]" src={back} alt="back arrow icon"></img> 
+        </Link> 
+        <div className='flex flex-col text-center'>
+            <h1 className='text-[100px]'>:-(</h1>
+            <h2 className='text-[40px]'>Oops! Something went wrong.</h2>
+            <p>Try refreshing the page later.</p>
+        </div>
     </>
   )
 }
