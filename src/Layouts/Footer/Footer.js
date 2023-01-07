83fe6ffb96ec746a51d93d2b19614c2d9c6acbdf
John Davies <jd.other@hotmail.com>
2023-01-07 18:54:40 +0000

    tablet design implemented

--- src/Layouts/Footer/Footer.js
+++ src/Layouts/Footer/Footer.js
@@ -4,7 +4,7 @@ import github from '../../Assets/Images/Icons/github.svg'
 
 const Footer = () => {
   return (
-    <div className='w-full h-[120px] bg-neutral-2 flex justify-center items-center'>
+    <footer className='w-full h-[120px] bg-neutral-2 flex justify-center items-center'>
         <div className='flex text-primary-3 items-center'>
             <a href="https://github.com/tigeryant" target="_blank" rel="noreferrer">
                 <img src={github} className='h-[27px] w-[27px] sm:h-[32px] sm:w-[32px]' alt='github icon' />
@@ -14,7 +14,7 @@ const Footer = () => {
             </a>
             <p className='ml-[3px] text-[12px] sm:text-[16px]'>Created by John Davies</p>
         </div>
-    </div>
+    </footer>
   )
 }
 
